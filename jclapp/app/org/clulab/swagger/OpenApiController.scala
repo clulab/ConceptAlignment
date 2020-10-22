3a2d86a0fa80fd3cbbef5c792e562fdf16655fdd
Keith Alcock <github@keithalcock.com>
2020-10-21 19:27:38 -0700

    Copy webapp

---
+++ jclapp/app/org/clulab/swagger/OpenApiController.scala
@@ -0,0 +1,14 @@
+package org.clulab.swagger.controllers
+
+import javax.inject._
+import play.api.mvc._
+import akka.actor._
+
+@Singleton
+class OpenApiController @Inject()(system: ActorSystem, cc: ControllerComponents)
+  extends AbstractController(cc) {
+
+  def openAPI() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.api())
+  }
+}

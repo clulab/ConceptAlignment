7ad504b80ee711edb9bc1a60b840dc902b8d1675
Keith Alcock <github@keithalcock.com>
2021-11-04 08:51:25 -0700

    Continue compositional

--- comparer/src/main/scala/org/clulab/alignment/comparer/CheckerApp.scala
+++ comparer/src/main/scala/org/clulab/alignment/comparer/CheckerApp.scala
@@ -7,6 +7,8 @@ import org.clulab.alignment.utils.Closer.AutoCloser
 import org.clulab.alignment.webapp.searcher.SearcherLocations
 import org.clulab.wm.eidoscommon.utils.StringUtils
 
+import scala.collection.mutable.ArrayBuffer
+
 object CheckerApp extends App {
   val ataInputFilename = "../comparer/ATA2.csv"
   val nafInputFilename = "../comparer/NAF2.csv"
@@ -25,7 +27,7 @@ object CheckerApp extends App {
   while (!searcher.isReady)
     Thread.sleep(100)
 
-  val conceptMap = {
+  val conceptMapLong = {
     val conceptIndex = searcher.compositionalOntologyMapperOpt.get.conceptIndex
     val map = conceptIndex.map { flatOntologyAlignmentItem =>
       val nodeName = flatOntologyAlignmentItem.id.nodeName
@@ -35,6 +37,22 @@ object CheckerApp extends App {
 
       println(key)
       key -> nodeName // Map nodeName without / to one with or without.
+    }.toMap
+
+    map
+  }
+
+  val conceptMapShort = {
+    val conceptIndex = searcher.compositionalOntologyMapperOpt.get.conceptIndex
+    val map = conceptIndex.map { flatOntologyAlignmentItem =>
+      val nodeName = flatOntologyAlignmentItem.id.nodeName
+      val withoutSlash =
+          if (nodeName.endsWith("/")) StringUtils.beforeLast(nodeName, '/')
+          else nodeName
+      val key = StringUtils.afterLast(withoutSlash, '/', true)
+
+      println(key)
+      key -> nodeName
     }.toMap
 
     map
@@ -80,44 +98,82 @@ object CheckerApp extends App {
     }
   }
 
-  def getHomeIdOpt(node: String, searcher: Searcher): Option[CompositionalOntologyIdentifier] = {
+  def getNodeName(node: String, homeId: CompositionalOntologyIdentifier, awayIdOpt: Option[CompositionalOntologyIdentifier]): String = {
+    val arrayBuffer = new ArrayBuffer[String]()
+
+    arrayBuffer += homeId.conceptOntologyIdentifier.nodeName
+    homeId.conceptPropertyOntologyIdentifierOpt.map(arrayBuffer += _.nodeName)
+    homeId.processOntologyIdentifierOpt.map(arrayBuffer += _.nodeName)
+    assert(homeId.processPropertyOntologyIdentifierOpt.isEmpty)
+    awayIdOpt.map(arrayBuffer += _.conceptOntologyIdentifier.nodeName)
+
+    val composedName = arrayBuffer
+      .map { string =>
+        val withoutSlash = if (string.endsWith("/")) string.dropRight(1) else string
+        StringUtils.afterLast(withoutSlash, '/', all = true)
+      }
+      .mkString("_")
+    val actualName = StringUtils.afterLast(node, '/', all = true)
+
+    if (composedName != actualName)
+      println("On ho!")
+    arrayBuffer.mkString(" ")
+  }
+
+  def getHomeIdOpt(node: String, searcher: Searcher): Option[(CompositionalOntologyIdentifier, Option[CompositionalOntologyIdentifier])] = {
     val parent = StringUtils.beforeLast(node, '/', all = false)
     val name = StringUtils.afterLast(node, '/', all = true)
     val parts = name.split('_')
     val count = parts.length
-    val homeIds = 0.until(count).flatMap { propertyCount =>
+    val homeIdAndAwayIdOpts = 0.until(count).flatMap { propertyCount =>
       val conceptParts = parts.dropRight(propertyCount)
-      val conceptPathOpt = conceptMap.get(parent + "/" + conceptParts.mkString("_"))
+      val conceptPathLongOpt = conceptMapLong.get(parent + "/" + conceptParts.mkString("_"))
 
       val otherParts = parts.takeRight(propertyCount)
+      val conceptPathShortOpt = conceptMapShort.get(otherParts.mkString("_"))
       val propertyPathOpt = propertyMap.get(otherParts.mkString("_"))
       val processPathOpt = processMap.get(otherParts.mkString("_"))
+      val count = Seq(conceptPathShortOpt, propertyPathOpt, processPathOpt).count(_.isDefined)
 
-      if (conceptPathOpt.isDefined && (otherParts.isEmpty || propertyPathOpt.isDefined || processPathOpt.isDefined)) {
-        val conceptIdentifier = FlatOntologyIdentifier(CompositionalOntologyIdentifier.ontology, conceptPathOpt.get, Some(CompositionalOntologyIdentifier.concept))
-        val homeIdOpt = {
-          if (otherParts.isEmpty)
-            Some(new CompositionalOntologyIdentifier(conceptIdentifier, None, None, None))
-          else if (propertyPathOpt.isDefined) {
-            val propertyIdentifier = FlatOntologyIdentifier(CompositionalOntologyIdentifier.ontology, propertyPathOpt.get, Some(CompositionalOntologyIdentifier.property))
-            Some(new CompositionalOntologyIdentifier(conceptIdentifier, Some(propertyIdentifier), None, None))
-          }
-          else if (processPathOpt.isDefined) {
-            val processIdentifier = FlatOntologyIdentifier(CompositionalOntologyIdentifier.ontology, processPathOpt.get, Some(CompositionalOntologyIdentifier.process))
-            Some(new CompositionalOntologyIdentifier(conceptIdentifier, None, Some(processIdentifier), None))
-          }
-          else
-            None
-        }
-
-        homeIdOpt
-      }
-      else
+      val homeIdOptAndAwayIdOptOpt = if (count > 1) {
+        println("Even worse!")
         None
+      }
+      else {
+        if (conceptPathLongOpt.isDefined && (otherParts.isEmpty || conceptPathShortOpt.isDefined || propertyPathOpt.isDefined || processPathOpt.isDefined)) {
+          val conceptIdentifier = FlatOntologyIdentifier(CompositionalOntologyIdentifier.ontology, conceptPathLongOpt.get, Some(CompositionalOntologyIdentifier.concept))
+          val (homeIdOpt, awayIdOpt) = {
+            if (otherParts.isEmpty)
+              (Some(new CompositionalOntologyIdentifier(conceptIdentifier, None, None, None)), None)
+            else if (conceptPathShortOpt.isDefined) {
+              val conceptShortIdentifier = FlatOntologyIdentifier(CompositionalOntologyIdentifier.ontology, conceptPathShortOpt.get, Some(CompositionalOntologyIdentifier.concept))
+              val homeIdOpt = Some(new CompositionalOntologyIdentifier(conceptIdentifier, None, None, None))
+              val awayIdOpt = Some(new CompositionalOntologyIdentifier(conceptShortIdentifier, None, None, None))
+              (homeIdOpt, awayIdOpt)
+            }
+            else if (propertyPathOpt.isDefined) {
+              val propertyIdentifier = FlatOntologyIdentifier(CompositionalOntologyIdentifier.ontology, propertyPathOpt.get, Some(CompositionalOntologyIdentifier.property))
+              (Some(new CompositionalOntologyIdentifier(conceptIdentifier, Some(propertyIdentifier), None, None)), None)
+            }
+            else if (processPathOpt.isDefined) {
+              val processIdentifier = FlatOntologyIdentifier(CompositionalOntologyIdentifier.ontology, processPathOpt.get, Some(CompositionalOntologyIdentifier.process))
+              (Some(new CompositionalOntologyIdentifier(conceptIdentifier, None, Some(processIdentifier), None)), None)
+            }
+            else
+              (None, None)
+          }
+
+          if (homeIdOpt.isEmpty && awayIdOpt.isEmpty) None
+          else Some(homeIdOpt.get, awayIdOpt)
+        }
+        else
+          None
+      }
+      homeIdOptAndAwayIdOptOpt
     }
-    val homeIdOpt = homeIds.find { homeId =>
+    val homeIdAndAwayIdOptOpt = homeIdAndAwayIdOpts.find { case (homeId, awayIdOpt) =>
       try {
-        searcher.run(homeId, Array.empty[CompositionalOntologyIdentifier], hits, thresholdOpt)
+        searcher.run(homeId, awayIdOpt.toArray, hits, thresholdOpt)
         true
       }
       catch {
@@ -125,7 +181,7 @@ object CheckerApp extends App {
       }
     }
 
-    homeIdOpt
+    homeIdAndAwayIdOptOpt
   }
 
   inputFilenames.zipWithIndex.foreach { case (inputFilename, index) =>
@@ -135,33 +191,38 @@ object CheckerApp extends App {
       val csvWriter = new CsvWriter(printWriter)
 
       nodes.foreach { node =>
-        val variableIds =
+        val nodeNameAndVariableIds =
             if (node.startsWith("wm/")) {
               val nodes = node.split('/')
               if (nodes.lift(1).map(_ == "concept").getOrElse(false)) {
-                val homeIdOpt = getHomeIdOpt(node, searcher)
+                val homeIdAndAwayIdOptOpt = getHomeIdOpt(node, searcher)
 
-                homeIdOpt
-                    .map { homeId =>
-                      searcher
-                          .run(homeId, Array.empty[CompositionalOntologyIdentifier], hits, thresholdOpt)
+                homeIdAndAwayIdOptOpt
+                    .map { case (homeId, awayIdOpt) =>
+                      val variableIds = searcher
+                          .run(homeId, awayIdOpt.toArray, hits, thresholdOpt)
                           .dstResults
                           .map { case (datamartIdentifier, _) => datamartIdentifier.variableId }
+                      (getNodeName(node, homeId, awayIdOpt), variableIds)
                     }
                     .getOrElse {
                       println(node)
-                      Seq("[Ontology node not found]")
+                      (node, Seq("[Ontology node not found]"))
                     }
               }
               else
-                Seq("[Not concept node]")
+                (node, Seq("[Not concept node]"))
             }
-            else
-              searcher
+            else {
+              val variableIds = searcher
                   .run(node, hits, thresholdOpt)
                   .map { case (datamartDocument, _ ) => datamartDocument.variableId }
 
-        csvWriter.println(Seq(node) ++ variableIds.take(2))
+              (node, variableIds)
+            }
+
+        val (nodeName, variableIds) = nodeNameAndVariableIds
+        csvWriter.println(Seq(nodeName) ++ variableIds.take(2))
       }
     }
   }

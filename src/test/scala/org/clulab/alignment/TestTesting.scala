fdd6dbaa2817e1a379ca996e7c9f79fb3957b526
Keith Alcock <github@keithalcock.com>
2021-12-02 17:01:57 -0700

    Test better

---
+++ src/test/scala/org/clulab/alignment/TestTesting.scala
@@ -0,0 +1,13 @@
+package org.clulab.alignment
+
+class TestTesting extends Test {
+
+  behavior of "Testing"
+
+  it should "pass a test" in {
+    true should be (true)
+  }
+
+  ignore should "ignore a test" in {
+  }
+}

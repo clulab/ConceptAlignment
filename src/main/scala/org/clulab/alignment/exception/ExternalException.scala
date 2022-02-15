16a736f973d5d2e51a5e3a2259792fb042934830
Keith Alcock <github@keithalcock.com>
2022-02-15 08:08:49 -0700

    Add exceptions

---
+++ src/main/scala/org/clulab/alignment/exception/ExternalException.scala
@@ -0,0 +1,3 @@
+package org.clulab.alignment.exception
+
+class ExternalException(message: String, cause: Throwable = null) extends AlignmentException(message, cause)
---
+++ src/main/scala/org/clulab/alignment/exception/InternalException.scala
@@ -0,0 +1,3 @@
+package org.clulab.alignment.exception
+
+class InternalException(message: String) extends AlignmentException(message)

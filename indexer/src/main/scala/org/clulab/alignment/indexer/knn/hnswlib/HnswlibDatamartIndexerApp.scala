0e39b856611dc2f91976543392b3446e0d4d9cc4
Keith Alcock <github@keithalcock.com>
2022-02-17 21:35:47 -0700

    Provide one-line example of how to not go through the app

--- indexer/src/main/scala/org/clulab/alignment/indexer/knn/hnswlib/HnswlibDatamartIndexerApp.scala
+++ indexer/src/main/scala/org/clulab/alignment/indexer/knn/hnswlib/HnswlibDatamartIndexerApp.scala
@@ -3,10 +3,8 @@ package org.clulab.alignment.indexer.knn.hnswlib
 object HnswlibDatamartIndexerApp extends App {
 
   def run(datamartFileName: String, indexFilename: String): Unit = {
-    val hnswlibIndexer = new HnswlibIndexer()
-
-    hnswlibIndexer.indexDatamart(datamartFileName, indexFilename)
+    new HnswlibIndexer().indexDatamart(datamartFileName, indexFilename)
   }
 
-  run(args(0), args(1))
+   run(args(0), args(1))
 }

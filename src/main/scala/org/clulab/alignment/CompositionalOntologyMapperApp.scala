075e86943e2af2f99123a0418240a284c7c40612
Keith Alcock <github@keithalcock.com>
2022-02-17 22:04:12 -0700

    Separate out Combiners

--- src/main/scala/org/clulab/alignment/CompositionalOntologyMapperApp.scala
+++ src/main/scala/org/clulab/alignment/CompositionalOntologyMapperApp.scala
@@ -1,7 +1,7 @@
 package org.clulab.alignment
 
 import com.github.jelmerk.knn.scalalike.SearchResult
-import org.clulab.alignment.data.Normalizer
+import org.clulab.alignment.combiner.{ContextAndOntologyCombiner, HomeAndAwayVectorCombiner}
 import org.clulab.alignment.data.Tokenizer
 import org.clulab.alignment.data.datamart.DatamartIdentifier
 import org.clulab.alignment.data.ontology.{CompositionalOntologyIdentifier, FlatOntologyIdentifier}
@@ -231,96 +231,6 @@ class HomeAndAwayOntologyScorer(homeWeight: Float, awayWeight: Float,
     val prod = score.prod()
 
     prod
-  }
-}
-
-class HomeAndAwayVectorCombiner(val homeWeight: Float, val awayWeight: Float,
-    val conceptWeight: Float, val conceptPropertyWeight: Float, val processWeight: Float, val processPropertyWeight: Float) {
-  require(homeWeight > 0)
-  require(conceptWeight > 0)
-
-  val homeAwaySum: Float = homeWeight + awayWeight
-  val homeFactor: Float = homeWeight / homeAwaySum
-  // This will be further divided by the number of away values so that they don't overwhelm the home value.
-  val awayFactor: Float = awayWeight / homeAwaySum
-  val weightSum: Float = conceptWeight + conceptPropertyWeight + processWeight + processPropertyWeight
-
-  def norm(array: Array[Float]): Array[Float] = {
-    val len = mul(array, array).sum
-    val result =
-        if (len == 0f) array
-        else mul(array, math.sqrt(1f / len).toFloat)
-
-    val len2 = mul(result, result).sum
-    result
-  }
-
-  def mul(array: Array[Float], factor: Float): Array[Float] = array.map(_ * factor)
-
-  def mulOpt(arrayOpt: Option[Array[Float]], factor: Float): Option[Array[Float]] =
-    arrayOpt.map(array => mul(array, factor))
-
-  def mul(left: Array[Float], right: Array[Float]): Array[Float] =
-    left.zip(right).map {
-      case (left, right) => left * right
-    }
-
-  def add(left: Array[Float], right: Array[Float]): Array[Float] =
-      left.zip(right).map {
-        case (left, right) => left + right
-      }
-
-  def addOpt(left: Array[Float], rightOpt: Option[Array[Float]]): Array[Float] = {
-    rightOpt.map { right =>
-      left.zip(right).map {
-        case (left, right) => left + right
-      }
-    }.getOrElse(left)
-  }
-
-  def combine(conceptVector: Array[Float], conceptPropertyVectorOpt: Option[Array[Float]],
-      processVectorOpt: Option[Array[Float]], processPropertyVectorOpt: Option[Array[Float]]): Array[Float] = {
-    val weightSum1 = conceptWeight
-    val weightSum2 = weightSum1 + conceptPropertyVectorOpt.map(_ => conceptPropertyWeight).getOrElse(0f)
-    val weightSum3 = weightSum2 +         processVectorOpt.map(_ =>         processWeight).getOrElse(0f)
-    val weightSum4 = weightSum3 + processPropertyVectorOpt.map(_ => processPropertyWeight).getOrElse(0f)
-    val weightSum = weightSum4
-
-    val step1 = mul(conceptVector, conceptWeight / weightSum)
-    val step2 = addOpt(step1, mulOpt(conceptPropertyVectorOpt, conceptPropertyWeight / weightSum))
-    val step3 = addOpt(step2, mulOpt(        processVectorOpt,         processWeight / weightSum))
-    val step4 = addOpt(step3, mulOpt(processPropertyVectorOpt, processPropertyWeight / weightSum))
-    val combined = step4
-    val normalized = norm(combined)
-
-    normalized
-  }
-
-  def combine(homeVector: Array[Float], awayVectors: Array[Array[Float]]): Array[Float] = {
-    if (awayVectors.nonEmpty) {
-      val totalAwayVector = awayVectors.tail.foldLeft(awayVectors.head) { (sum, score) => add(sum, score) }
-      val awayVector = mul(totalAwayVector, 1f / awayVectors.length)
-      val homeAndAwayVector = add(mul(homeVector, homeFactor), mul(awayVector, awayFactor))
-      val normalized = norm(homeAndAwayVector)
-
-      normalized
-    }
-    else
-      homeVector
-  }
-}
-
-class ContextAndOntologyCombiner(contextWeight: Float, ontologyWeight: Float) {
-  val normalizer = Normalizer()
-
-  def combine(contextVector: Array[Float], ontologyVector: Array[Float]): Array[Float] = {
-    val weightedContext = contextVector.map(_ * contextWeight)
-    val weightedOntology = ontologyVector.map(_ * ontologyWeight)
-    val combined = Array(weightedContext, weightedOntology).transpose.map(_.sum)
-    // This throws an exception if the vector could not be normalized!
-    val normalized = normalizer.normalize(combined).get
-
-    normalized
   }
 }
 
